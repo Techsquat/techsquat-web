ea7cc2cc966ba9852f2a0cac431f282983fb26e1
pageonline <ondrejpagebarta@gmail.com>
2017-02-18 18:49:17 +0100

    add highlighted anchor

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 $(document).foundation();
 
 
+var updateHash = function() {
+	var highlightElement;
+	var highlightClassName = 'hash-highlight';
+	var highlights = document.querySelectorAll('.' + highlightClassName);
+
+	for (var i = 0; i < highlights.length; ++i) {
+		highlights[i].classList.remove('.' + highlightClassName);
+	}
+	if (location.hash) {
+		highlightElement = document.querySelector(location.hash);
+		highlightElement.classList.add(highlightClassName);
+		setTimeout(function() {
+			scrollTo(0, highlightElement.offsetTop - 100);
+		}, 1);
+	}
+}
+
+addEventListener('hashchange', updateHash);
+addEventListener('DOMContentLoaded', updateHash);
+addEventListener('load', updateHash);
